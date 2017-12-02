5d98e5cd0f4f7587d565dda70d9a8dc8c1d00441
electriwizard <e1ectricwiz4rd@gmail.com>
2017-12-02 16:48:08 +0300

    added HTTP-server and video edit

---
+++ SimpleHTTPServer.py
@@ -0,0 +1,12 @@
+# C:/Python34/python.exe
+# -*- coding: utf-8 -*-
+
+import http.server
+import socketserver
+
+port = 8080
+handler = http.server.SimpleHTTPRequestHandler
+
+httpd = socketserver.TCPServer(("", port), handler)
+
+httpd.serve_forever()
